e67f4664fe3ea9fb9bf8da0d3c3e85de48d12889
Jesse Russell <jesse-russell@users.noreply.github.com>
2017-06-11 17:09:10 -0400

    Create build.sh

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ virtualenv venv
 source venv/bin/activate
 pip --no-cache-dir install -r requirements.txt
 npm install -g bower
-bower install
+bower --allow-root install
 python manage.py makemigrations
 python manage.py migrate
