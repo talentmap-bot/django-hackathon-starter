9d3224c4436eedbf586b465de9f1a6f09423ff57
Jesse Russell <jesse-russell@users.noreply.github.com>
2017-06-12 11:38:29 -0400

    Create build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-sleep 3000
+virtualenv venv
+source venv/bin/activate
+pip --no-cache-dir install -r requirements.txt
+npm install -g bower
+bower --allow-root install
+python hackathon_starter/manage.py makemigrations
+python hackathon_starter/manage.py migrate
